eff4f02a58a014cd6de04eecde3ed6ae79b92802
sbstjn <mail@sbstjn.com>
2017-05-04 22:26:08 +0200

    Check if tsfix is installed into node_modules folder

--- scripts/link.sh
+++ scripts/link.sh
@@ -4,6 +4,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create link for tsfix/tslint.json
-if [ -w $DIR/../../../tslint.json ] then;
+if [[ "$DIR" = */node_modules/* ]]; then
   ln -s node_modules/tsfix/tslint.json $DIR/../../../tslint.json
 fi

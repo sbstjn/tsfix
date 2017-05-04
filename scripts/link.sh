0fbc5a47ec4a9f66b4e2514b2979cff3ba7e9e5d
sbstjn <mail@sbstjn.com>
2017-05-04 22:24:45 +0200

    Check for permission to create link

--- scripts/link.sh
+++ scripts/link.sh
@@ -4,4 +4,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create link for tsfix/tslint.json
-ln -s node_modules/tsfix/tslint.json $DIR/../../../tslint.json
+if [ -w $DIR/../../../tslint.json ] then;
+  ln -s node_modules/tsfix/tslint.json $DIR/../../../tslint.json
+fi
